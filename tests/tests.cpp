2930499268173adaa274d535be84a6f3895516d3
Sergey Lobanov <lobanov199969@gmail.com>
2021-05-21 15:39:22 +0300

    fix

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -144,7 +144,7 @@ int main()
     TestStruct* data = new TestStruct[N];
     lab618::CHash<TestStruct, hash, Compare> Table(13, 2);*/
 
-    for (int i = 0; i < size - 1; ++i)
+    for (int i = 0; i < size; ++i)
     {
         int N = Ns[i];
         
